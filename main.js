65461aa44864bbd87894593bd44c7590e9c2cdd7
diankaloza <diankaloza@gmail.com>
2022-06-30 16:46:25 +0300

    filejs

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
-let a = '';
-let b = '';
-let sign = '';
+let a = "";
+let b = "";
+let sign = "";
 let finish = false;
 
-const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","."];
+const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 const action = ["х", "-", "/", "%"];
 
 const out = document.querySelector(".calc-screen p");
 
 function allClear() {
-    a = "";
-    b = "";
-    sign = "";
-    let finish = false;
-    out.textContent = 0;
+  a = "";
+  b = "";
+  sign = "";
+  let finish = false;
+  out.textContent = 0;
 }
 
+document.querySelector(".ac").onclick = allClear;
 
-document.querySelector('.ac').onclick = allClear;
+document.querySelector(".buttons").onclick = (event) => {
+  if (!event.target.classList.contains("btn")) return;
+  if (!event.target.classList.contains(".ac")) return;
 
+  out.textContent = "";
+
+  const key = event.target.textContent;
+
+  if (digit.includes(key)) {
+    a += key;
+    console.log(a, b, sign);
+  }
+};
