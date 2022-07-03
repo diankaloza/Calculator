140eb227860b239c10d50ec7dc24c8c7596b456e
diankaloza <diankaloza@gmail.com>
2022-07-03 04:35:45 +0300

    all changes

--- main.js
+++ main.js
@@ -12,39 +12,48 @@ function allClear() {
   a = "";
   b = "";
   sign = "";
-  let finish = false;
+  finish = false;
   out.textContent = 0;
 }
 
 document.querySelector(".ac").onclick = allClear;
 
 document.querySelector(".buttons").onclick = (event) => {
-  if (!event.target.classList.contains("btn")) return;
-  if (event.target.classList.contains(".ac")) return;
+  const button = event.target;
+  if (!button.classList.contains("btn")) return;
+  if (button.classList.contains("ac")) return;
 
   out.textContent = "";
 
-  const key = event.target.textContent;
+  // значение которое мы берем из нажатой кнопки
+  const key = button.textContent;
 
   if (digit.includes(key)) {
     if (b === "" && sign === "") {
       a += key;
-
+      console.log(a, b, sign);
       out.textContent = a;
+      //если мы хотим сделать еще одно действие после того, как нажали '='
     } else if (a !== "" && b !== "" && finish) {
+      b = key;
+      finish = false;
+      out.textContent = b;
+      console.log(a, b, sign);
     } else {
       b += key;
       out.textContent = b;
+      console.log(a, b, sign);
     }
   }
   if (action.includes(key)) {
     sign = key;
     out.textContent = sign;
-
+    console.log(a, b, sign);
     return;
   }
 
   if (key === "=") {
+    if (b === "") b = a;
     switch (sign) {
       case "+":
         a = +a + +b;
@@ -58,8 +67,18 @@ document.querySelector(".buttons").onclick = (event) => {
         a = a * b;
         break;
 
+      case "%":
+        a = a / 100;
+        break;
+
       case "/":
         a = a / b;
+        // если деление на 0 (выбивает infinity)
+        if (b === "0") out.textContent = "Ошибка";
+        a = "";
+        b = "";
+        sign = "";
+        return;
     }
     finish = true;
     out.textContent = a;
