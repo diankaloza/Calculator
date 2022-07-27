7e2b56910a790c6dd8f09b1e770b95c98ad9e852
diankaloza <diankaloza@gmail.com>
2022-07-27 20:23:26 +0300

    js

---
+++ css/main.js
@@ -0,0 +1,91 @@
+let a = "";
+let b = "";
+let sign = "";
+let finish = false;
+
+const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const action = ["х", "-", "/", "%", "+"];
+
+const out = document.querySelector(
+  ".col-12 text-end p-3 bg-primary bg-opacity-25 fs-1 fw-bold"
+);
+
+function allClear() {
+  a = "";
+  b = "";
+  sign = "";
+  finish = false;
+  out.textContent = 0;
+}
+
+document.querySelector(".ac").onclick = allClear;
+
+document.querySelector(".btn").onclick = (event) => {
+  const btn = event.target;
+  if (!btn.classList.contains("btn")) return;
+  if (btn.classList.contains("ac")) return;
+  out.textContent = "";
+
+  // значение которое мы берем из нажатой кнопки
+  const key = btn.textContent;
+
+  if (digit.includes(key)) {
+    if (b === "" && sign === "") {
+      a += key;
+      console.log(a, b, sign);
+      out.textContent = a;
+      //если мы хотим сделать еще одно действие после того, как нажали '='
+    } else if (a !== "" && b !== "" && finish) {
+      b = key;
+      finish = false;
+      out.textContent = b;
+      console.log(a, b, sign);
+    } else {
+      b += key;
+      out.textContent = b;
+      console.log(a, b, sign);
+    }
+  }
+  if (action.includes(key)) {
+    sign = key;
+    out.textContent = sign;
+    console.log(a, b, sign);
+    return;
+  }
+
+  if (key === "=") {
+    if (b === "") b = a;
+    switch (sign) {
+      case "+":
+        a = +a + +b;
+        break;
+
+      case "-":
+        a = a - b;
+        break;
+
+      case "х":
+        a = a * b;
+        break;
+
+      case "%":
+        a = a / 100;
+        break;
+
+      case "/":
+        // если деление на 0 (выбивает infinity)
+        if (b === "0") {
+          out.textContent = "Ошибка";
+          a = "";
+          b = "";
+          sign = "";
+          return;
+        }
+        a = a / b;
+        break;
+    }
+    finish = true;
+    out.textContent = a;
+    console.log(a, b, sign);
+  }
+};
